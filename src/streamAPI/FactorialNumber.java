be66e5268d629275d725be82f451e3f48c70c16e
Amruta <amrutardevkar1@gmail.com>
2024-03-12 21:05:38 +0530

    new class

---
+++ src/streamAPI/FactorialNumber.java
@@ -0,0 +1,25 @@
+package streamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Integer [] arr = {1,2,3,4};
+		List<Integer> list = Arrays.asList(arr);
+		list.stream().map((n) -> {
+			
+			 Integer factor=1;
+			 for(int i=1; i<=n; i++)
+			 {
+				 factor= factor*i;
+			 }
+			 
+			 return factor;
+		}).forEach(System.out::println);
+	}
+
+}
