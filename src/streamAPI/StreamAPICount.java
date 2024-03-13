d1fa4be598f3012c8cc15c0feafc1bfbd5efdce9
Amruta <amrutardevkar1@gmail.com>
2024-03-13 12:43:39 +0530

    changes done

--- src/streamAPI/StreamAPICount.java
+++ src/streamAPI/StreamAPICount.java
@@ -7,9 +7,10 @@ public class StreamAPICount{
 
  public static void main(String[] args)
   {
-    Integer[] an ={1,2,3,4,5,6,7,8,9,10};
+    Integer[] an ={1,2,3,4,5,6,7,8,9,10,11,12};
     List<Integer> listn = Arrays.asList(an);
 
-    long count = listn.stream().filter((n) -> {n%3 ==0;}).count();
+    long count = listn.stream().filter((n) ->  (n % 3)==0).count();
+    System.out.println("Count of numbers divisible by 3: " + count);
   }
 }
--- src/streamAPI/StreamAPIPractice.java
+++ src/streamAPI/StreamAPIPractice.java
@@ -7,10 +7,12 @@ import java.util.stream.Collectors;
 public class StreamAPIPractice{
 public static void main(String[] args)
 {
-   String[] ar ={"amruta", "pratima","kajal,"Amu","Gau"};
+   String[] ar ={"amruta", "pratima","kajal","Amu","Gau"};
    List<String> list = Arrays.asList(ar);
-   List<String> sorted = list.stream().sorted().collect(Collector.toList());
+   List<String> sorted = list.stream().sorted().collect(Collectors.toList());
+   System.out.println("Sorted list is: " + sorted);
    
-   List<String> reverse = list.stream().sorted(Comparator.reverseOrder()).collect(Collector.toList());
+   List<String> reverse = list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+   System.out.println("Reverse sorted list is: " + reverse);
 }
 }
