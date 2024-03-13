346b516c4d4973cb43e286dba93fcb7f7fa2e5d2
amrutardevkar1 <156201921+amrutardevkar1@users.noreply.github.com>
2024-03-13 08:19:29 +0530

    Update FactorialNumber.java
    Added new line of code

--- src/streamAPI/FactorialNumber.java
+++ src/streamAPI/FactorialNumber.java
@@ -12,6 +12,14 @@ public class FactorialNumber {
 		Integer [] arr = {1,2,3};
 		
 		List<Integer> list = Arrays.asList(arr);
+		list.stream().((n) -> {
+			                 Integer factor=1;
+			                  for(inti=1; i<=n; i++)
+						  {
+						     factor= factor*i;
+						  }
+			                  return factor;
+	                        	} ).forEach(System.out::println);
 	
 	}
 
