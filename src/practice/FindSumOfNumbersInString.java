9c703d662d026836709e71db76e6d4b4ec590184
Amruta <amrutardevkar1@gmail.com>
2024-03-22 15:16:18 +0530

    Com

--- src/practice/FindSumOfNumbersInString.java
+++ src/practice/FindSumOfNumbersInString.java
@@ -5,21 +5,14 @@ public class FindSumOfNumbersInString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-		
-    
-        System.out.println(sumOfDigits("Amruta1221Devkar#gau21"));
-	}
-
-	public static int sumOfDigits(String s)
-	{
+		String s ="Amruta1221Devkar#gau21";
 		String s1=s.substring(6,9);
 		String s2 =s.substring(20);
 		String str= s1+s2;
         Integer n = Integer.valueOf(str);
         int n1= n;
         System.out.println(n);
-        int sum=0;
+         int sum=0;
      while(n1>0)
      {
     	
@@ -29,7 +22,8 @@ public class FindSumOfNumbersInString {
          n1=n1/10;
          
      }
-     
-     return sum;
+    
+        System.out.println(sum);
 	}
+
 }
