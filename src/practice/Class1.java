c867a469d6bb08ac124d3c29cf59d1a4efd1f51e
Amruta <amrutardevkar1@gmail.com>
2024-03-24 13:32:44 +0530

    done

--- src/practice/Class1.java
+++ src/practice/Class1.java
@@ -5,10 +5,6 @@ public class Class1 {
 	public static void main(String [] args)
 	{
 		String s="my name is amruta";
-		String s2 ="its my life";
-		String s3 ="this is way";
-		String s4="new file is ready";
-		String s5 ="adsvggegff";
-	    
+			    
 	}
 }
